d0ea36cbb2546de5ffd3bbeca770aeee347de0c5
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-03-04 12:07:41 -0300

    Added xkcd

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
    '("e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" default))
  '(evil-undo-system 'undo-redo)
  '(package-selected-packages
-   '(pdf-tools zone-nyan yaml-mode use-package unicode-fonts terminal-here smex rust-mode rg ranger powerline org-superstar org-noter markdown-mode magit lua-mode ligature lice julia-mode htmlize hl-todo haskell-mode flx fireplace expand-region evil-org evil-numbers evil-mc evil-commentary evil-collection editorconfig doom-themes doom-modeline diredfl dashboard darkroom counsel-projectile all-the-icons-ivy all-the-icons-dired))
+   '(xkcd pdf-tools zone-nyan yaml-mode use-package unicode-fonts terminal-here smex rust-mode rg ranger powerline org-superstar org-noter markdown-mode magit lua-mode ligature lice julia-mode htmlize hl-todo haskell-mode flx fireplace expand-region evil-org evil-numbers evil-mc evil-commentary evil-collection editorconfig doom-themes doom-modeline diredfl dashboard darkroom counsel-projectile all-the-icons-ivy all-the-icons-dired))
  '(pdf-tools-enabled-modes
    '(pdf-history-minor-mode pdf-isearch-minor-mode pdf-links-minor-mode pdf-misc-minor-mode pdf-outline-minor-mode pdf-misc-size-indication-minor-mode pdf-misc-menu-bar-minor-mode pdf-annot-minor-mode pdf-sync-minor-mode pdf-misc-context-menu-minor-mode pdf-cache-prefetch-minor-mode pdf-occur-global-minor-mode pdf-virtual-global-minor-mode))
  '(warning-suppress-types '((use-package) (use-package))))
