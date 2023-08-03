4a0a4f611b64f217566b5bbb1a4e71d84ce97537
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-08-02 22:36:41 -0300

    Removed racket mode

--- init.el
+++ init.el
@@ -130,11 +130,6 @@
 (use-package coffee-mode)
 (use-package julia-mode)
 (use-package tuareg) ;; OCaml
-(use-package racket-mode
-  :config
-  (add-hook 'racket-mode-hook #'(lambda ()
-				  (setq prettify-symbols-alist '(("lambda" . 955)))
-				  (prettify-symbols-mode))))
 (use-package toml-mode)
 (use-package yaml-mode)
 (use-package erlang)
