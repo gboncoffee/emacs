db65659674403ceaf692b42698e377adcfab062f
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-08-07 20:58:38 -0300

    Returned to elea, light theme did not work

--- init.el
+++ init.el
@@ -16,9 +16,9 @@
 ;;
 ;; appearance
 ;;
-(use-package acme-theme
+(use-package ef-themes
   :init
-  (load-theme 'acme t))
+  (load-theme 'ef-elea-dark t))
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
@@ -255,12 +255,13 @@
  '(auth-source-save-behavior nil)
  '(markdown-header-scaling t)
  '(package-selected-packages
-   '(acme-theme editorconfig dante tuareg lice auctex dired-atool elixir-mode erlang haskell-mode julia-mode lua-mode magit markdown-mode pdf-tools rust-mode toml-mode xkcd yaml-mode rg multiple-cursors rainbow-mode go-mode use-package)))
+   '(ef-themes editorconfig dante tuareg lice auctex dired-atool elixir-mode erlang haskell-mode julia-mode lua-mode magit markdown-mode pdf-tools rust-mode toml-mode xkcd yaml-mode rg multiple-cursors rainbow-mode go-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ido-subdir ((t (:foreground "deep sky blue"))))
  '(org-level-1 ((t (:inherit outline-1 :extend nil :height 1.3))))
  '(org-level-2 ((t (:inherit outline-2 :extend nil :height 1.2))))
  '(org-level-3 ((t (:inherit outline-3 :extend nil :height 1.1)))))
